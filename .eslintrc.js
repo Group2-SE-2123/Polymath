c8173cb5b3f44abfcaf885fcf98a620e9b0024ea
Hans Daduya <hansdaduya@yahoo.com>
2021-11-18 00:50:55 +0800

    :building_construction: Add configuration tools (prettier, eslint, husky)
    Add configuration tools (prettier, eslint, husky)

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+	extends: ["airbnb-base", "prettier", "eslint:recommended"],
+	plugins: ["prettier"],
+	rules: {
+		semi: ["error", "always"],
+		quotes: ["error", "double"],
+	},
+};
