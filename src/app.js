ce9aa85bbc5dadf2650146fdc31d2d5dcb8984e5
Hans Daduya <hansdaduya@yahoo.com>
2021-11-22 13:54:43 +0800

    :goal_net: Create an error page
    Create an error page which redirects there if 404

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-import createError from "http-errors";
 import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
@@ -24,20 +23,9 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/terms", termsRouter);
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-	next(createError(404));
-});
-
 // error handler
-app.use((err, req, res) => {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get("env") === "development" ? err : {};
-
-	// render the error page
-	res.status(err.status || 500);
-	res.render("error");
+app.get("*", (req, res) => {
+	res.status(404).send("error");
 });
 
 export default app;
