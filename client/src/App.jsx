bfeef61d03c2f928d08cb756a6409f3220b8b956
Hans Daduya <hansdaduya@yahoo.com>
2021-11-25 21:02:15 +0800

    :zap: Refactor tailwind desings
    Refactor tailwind desings

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Header from "./components/Header";
+import OtherHeader from "./components/OtherHeader";
 
 function App() {
-	return <Header />;
+	return <OtherHeader />;
 }
 
 export default App;
