d1236279405a9b9fec6eca40f3a6457a6cadb5e2
Hans Daduya <hansdaduya@yahoo.com>
2021-11-25 16:36:53 +0800

    :building_construction: Create frontend folder using react + vite
    Create frontend folder to separate frontend from backend

--- src/app.js
+++ src/app.js
@@ -3,26 +3,15 @@ import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 
-import indexRouter from "./routes/index";
-import usersRouter from "./routes/users";
 import questionRouter from "./api/question";
-import termsRouter from "./routes/terms-of-service";
 
 const app = express();
-
-// view engine setup
-app.set("views", path.join(process.cwd(), "views"));
-app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(process.cwd(), "public")));
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/terms", termsRouter);
 app.use("/api", questionRouter);
 
 // error handler
--- src/routes/terms-of-service.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const termsRouter = express.Router();
-
-termsRouter.get("/", (req, res) => {
-	res.render("terms-of-service", { title: "Express" });
-});
-
-export default termsRouter;
