09c74d0f6065d7b5fb9dca4d793151ab883e8e19
Hans Daduya <hansdaduya@yahoo.com>
2021-12-03 00:43:33 +0800

    :boom: Add weird changes
    Add weird changes

--- quiz.js
+++ quiz.js
@@ -3,7 +3,7 @@ import prisma from "~/prisma/db";
 
 const quizRouter = express.Router();
 
-//GETS a list of  quizzes in the database//
+// GETS a list of  quizzes in the database//
 quizRouter.get("/getQuiz", async (req, res) => {
 	Promise.resolve()
 		.then(() => prisma.quiz.findMany())
@@ -11,23 +11,21 @@ quizRouter.get("/getQuiz", async (req, res) => {
 		.catch((err) => res.send(err));
 });
 
-//GETS the questions of a specific quizz//
+// GETS the questions of a specific quizz//
 quizRouter.get("/getQuizQuestions", async (req, res) => {
-    const quiz;
-
 	Promise.resolve()
-		.then(() => prisma.quiz.findUnique({
-            where:{
-                id: quiz,
-            },
-            select:{
-                question: true,
-            }
-        }))
+		.then(() =>
+			prisma.quiz.findUnique({
+				where: {
+					id: 1,
+				},
+				select: {
+					question: true,
+				},
+			})
+		)
 		.then((quiz) => res.json(quiz))
 		.catch((err) => res.send(err));
 });
-
-
 
 export default quizRouter;
--- src/routes/privacy_policy.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const privacyRouter = express.Router();
-
-privacyRouter.get("/", (req, res) => {
-	res.render("privacy_policy", { title: "Express" });
-});
-
-export default privacyRouter;
