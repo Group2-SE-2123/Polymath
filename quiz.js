5c36270e24a75ef893601cc2d7ec8e05e9b0fdd9
DelgadoArlee <84315721+DelgadoArlee@users.noreply.github.com>
2021-12-02 23:51:57 +0800

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,33 @@
+import express from "express";
+import prisma from "~/prisma/db";
+
+const quizRouter = express.Router();
+
+//GETS a list of  quizzes in the database//
+quizRouter.get("/getQuiz", async (req, res) => {
+	Promise.resolve()
+		.then(() => prisma.quiz.findMany())
+		.then((quiz) => res.json(quiz))
+		.catch((err) => res.send(err));
+});
+
+//GETS the questions of a specific quizz//
+quizRouter.get("/getQuizQuestions", async (req, res) => {
+    const quiz;
+
+	Promise.resolve()
+		.then(() => prisma.quiz.findUnique({
+            where:{
+                id: quiz,
+            },
+            select:{
+                question: true,
+            }
+        }))
+		.then((quiz) => res.json(quiz))
+		.catch((err) => res.send(err));
+});
+
+
+
+export default quizRouter;
