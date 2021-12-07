46bb31a5bc9fde0f071f6862dfa63025ac07383c
Hans Daduya <hansdaduya@yahoo.com>
2021-12-07 13:28:43 +0800

    :pencil2: Change from crlf to lf
    Change from crlf

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -84,19 +84,13 @@ function Login() {
 											type="checkbox"
 											className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
 										/>
-										<label
-											htmlFor="remember-me"
-											className="ml-2 block text-sm text-gray-900"
-										>
+										<label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
 											Remember me
 										</label>
 									</div>
 
 									<div className="text-sm">
-										<a
-											href="#"
-											className="font-medium text-black-600 hover:text-black-500"
-										>
+										<a href="#" className="font-medium text-black-600 hover:text-black-500">
 											Forgot your password?
 										</a>
 									</div>
