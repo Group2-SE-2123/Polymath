699397ef74be58477060f785d3d542ab4178cad8
DelgadoArlee <delgadoarlee@gmail.com>
2021-12-10 19:37:12 +0800

    added quiz api and quiz controller

--- src/api/question.js
+++ src/api/question.js
@@ -2,8 +2,24 @@ import express from "express";
 import prisma from "~/prisma/db";
 
 import { getAllQuestions } from "../controller/question";
+import { createQuiz } from "../controller/quiz";
 
 const router = express.Router();
+
+router.post("/createQuiz",  async (req, res) => {
+	const quiz = req.body;
+
+	new Promise((resolve, reject) => {
+		if(quiz != null){
+			resolve(quiz);
+		}else{
+			reject(res.status(400));	
+		}
+	})
+	.then(createQuiz(quiz))
+	.then(()=> res.status(201).json(quiz))
+	.catch((err) => res.send(err));
+})
 
 router.post("/createQuestion", (req, res) => {
 	prisma.question.create({
---
+++ src/controller/quiz/index.js
@@ -0,0 +1,24 @@
+import prisma from "~/prisma/db";
+
+const createQuiz = async (json) => {
+    return prisma.quiz
+     .create({
+         data:{
+            userId:json.userId,
+            name:json.name,
+            length:json.length,
+            question: json.question
+         }
+     });
+};
+
+const getAllQuiz = () => {
+	return prisma.question.findMany();
+};
+
+
+
+
+
+
+export { createQuiz };
