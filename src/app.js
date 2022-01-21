5dcc591a9f45af3f13342d05f3eb5676087a89d2
Hans Daduya <hansdaduya@yahoo.com>
2022-01-21 14:22:15 +0800

    :bug: Fix route bugs
    Fix route bugs

--- src/app.js
+++ src/app.js
@@ -39,7 +39,7 @@ app.use("/auth", authRouter);
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("client/dist"));
 
-	app.get("*", (req, res) => {
+	app.get("/*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
 	});
 }
